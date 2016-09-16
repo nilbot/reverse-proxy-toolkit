aaa4640015247990ff1cb2b14fc54b1e00aedbdf
Ersi Ni <ersi.ni@nilbot.net>
2016-09-16 19:15:54 +0100

    Fix check, sorry my bash is very rusty TODO fix create_tun.sh -_-b

--- check_life.sh
+++ check_life.sh
@@ -9,9 +9,30 @@ fi
 SOCKET=$1
 HOST=$2
 
-RES=`ssh -S $SOCKET -O check $HOST 2>&1`
-if [ $? -eq 0 ]; then
-	export TUNPID=`print $RES | awk 'match($0,/[0-9]+/) { print substr($0, RSTART, RLENGTH) }'`
-else
-	exit 255
+if [ -z ${debug+"FALSE"} ]; then
+	:
 fi
+
+if [ "$debug" = "TRUE" ]; then
+	echo "socket_file: $SOCKET"
+	echo "host: $HOST"
+fi
+
+RES=`ssh -S $SOCKET -O check $HOST 2>&1`
+
+CHECK_RET=$?
+
+if [ "$debug" = "TRUE" ]; then
+	echo "RES: $RES"
+	echo "CHECK_RET: $CHECK_RET"
+fi
+
+if [ "$debug" = "TRUE" ]; then
+	echo "RES: $RES"
+fi
+
+if [ $CHECK_RET -ne 0 ]; then
+	exit $CHECK_RET
+fi
+
+echo $RES | awk 'match($0,/[0-9]+/) { print substr($0, RSTART, RLENGTH) }'
